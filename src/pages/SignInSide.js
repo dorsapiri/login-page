7de503c36c7896ad58362a205ac33f0995d821d2
dorsapiri <dorsapiri@gmail.com>
2022-09-14 17:56:26 +0430

    added signup and signin pages

---
+++ src/pages/SignInSide.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+
+
+
+export default function signIn(){
+    return(
+        <Container component="main">
+
+        </Container>
+    );
+}
